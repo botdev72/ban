de87bd27ddb2d6beb3da91f63499ba9b20f69633
dimastirtan <dimastirta027@gmail.com>
2024-03-05 15:29:50 +0700

    banall

---
+++ BanAllBot/modules/banall.py
@@ -0,0 +1,21 @@
+from BanAllBot import app,BOT_ID,SUDO
+from pyrogram import filters,enums
+
+
+@app.on_message(filters.command("banall") & filters.user(SUDO))
+async def ban_all(_,msg):
+    chat_id=msg.chat.id    
+    bot=await app.get_chat_member(chat_id,BOT_ID)
+    bot_permission=bot.privileges.can_restrict_members==True    
+    if bot_permission:
+        async for member in app.get_chat_members(chat_id):       
+            try:
+                    await app.ban_chat_member(chat_id, member.user.id)
+                    await msg.reply_text(f"𝐖ʟᴄ 𝐁ᴀʙʏ 😘❤️ {member.user.mention}")                    
+            except Exception:
+                pass
+    else:
+        await msg.reply_text("𝐀ʙᴇ 𝐋ᴏᴠᴅᴇ 𝐌ᴀᴛʜᴇʀᴄʜᴏᴅ 𝐁ʜᴀᴋ ")  
+                                         
+    
+            
